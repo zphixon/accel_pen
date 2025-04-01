64c7c741d184d4be2cd4043feae9226252ec62bf
Zack H <zphixon@gmail.com>
2025-04-01 16:00:03 -0400

    hmm

--- backend/schema.sql
+++ backend/schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE user (
     nadeo_login VARCHAR(128) UNIQUE NOT NULL,
     ubi_account_id CHAR(36) UNIQUE NOT NULL,
     site_admin BOOLEAN NOT NULL DEFAULT FALSE,
+    registered TIMESTAMP NOT NULL,
 
     PRIMARY KEY (user_id)
 );
@@ -14,9 +15,27 @@ CREATE TABLE map (
     gbx_data BLOB NOT NULL,
     mapname TEXT NOT NULL,
     author INTEGER UNSIGNED NOT NULL,
+    votes INTEGER NOT NULL DEFAULT 1,
+    uploaded TIMESTAMP NOT NULL,
+    created TIMESTAMP NOT NULL,
 
     PRIMARY KEY (ap_id),
     FOREIGN KEY (author)
         REFERENCES user (user_id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE vote (
+    user_id INTEGER UNSIGNED,
+    map_ap_id INTEGER UNSIGNED,
+    vote_value TINYINT NOT NULL,
+    cast TIMESTAMP NOT NULL,
+
+    PRIMARY KEY (user_id, map_ap_id),
+    FOREIGN KEY (user_id)
+        REFERENCES user (user_id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (map_ap_id)
+        REFERENCES map (ap_id)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
