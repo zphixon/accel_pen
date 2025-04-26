62846670405d735c829d4e9d495d194fc31244dd
Zack H <zphixon@gmail.com>
2025-04-26 16:01:58 -0400

    restructure

---
+++ backend/src/config.rs
@@ -0,0 +1,73 @@
+use std::{net::SocketAddr, path::PathBuf, sync::LazyLock};
+use url::Url;
+
+from_env::config!(
+    "ACCEL_PEN",
+    net {
+        domain: String,
+        #[serde(default)]
+        root: String,
+        bind: SocketAddr,
+        user_agent: String,
+        #[serde(default = "default_cors_host")]
+        cors_host: String,
+        frontend_url: Url,
+    },
+    db {
+        url: Url,
+        password_path: PathBuf,
+    },
+    nadeo {
+        identifier: String,
+        secret_path: PathBuf,
+        redirect_url: Url,
+    }
+);
+
+fn default_cors_host() -> String {
+    String::from("*")
+}
+
+impl Config {
+    pub fn route_v1(&self, path: &str) -> String {
+        format!("{}/v1/{}", self.net.root, path)
+    }
+}
+
+pub static CLIENT_SECRET: LazyLock<String> = LazyLock::new(|| {
+    let Ok(secret) = std::fs::read_to_string(&CONFIG.nadeo.secret_path) else {
+        panic!("Couldn't read nadeo client secret file");
+    };
+    secret
+});
+
+pub static CONFIG: LazyLock<Config> = LazyLock::new(|| {
+    let arg = std::env::args().nth(1).expect("need config filename arg");
+    let content = std::fs::read_to_string(arg).expect("could not read config file");
+
+    let mut config = toml::from_str::<Config>(&content).expect("invalid TOML");
+    config.hydrate_from_env();
+
+    assert!(
+        config.db.password_path.is_file(),
+        "DB password path must be file"
+    );
+
+    config
+        .db
+        .url
+        .set_username("root")
+        .expect("Couldn't set username on DB URL");
+
+    let Ok(password) = std::fs::read_to_string(&config.db.password_path) else {
+        panic!("Couldn't read DB password file");
+    };
+
+    config
+        .db
+        .url
+        .set_password(Some(password.trim()))
+        .expect("Couldn't set password on DB URL");
+
+    config
+});
---
+++ backend/src/error.rs
@@ -0,0 +1,203 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use serde::Serialize;
+use std::{fmt::Display, ops::Deref};
+use ts_rs::TS;
+
+#[derive(Debug, thiserror::Error, strum::IntoStaticStr, TS)]
+#[ts(export)]
+pub enum ApiErrorInner {
+    #[error("Database error: {0}")]
+    Database(
+        #[ts(skip)]
+        #[from]
+        sqlx::Error,
+    ),
+
+    #[error("Migration error: {0}")]
+    Migration(
+        #[ts(skip)]
+        #[from]
+        sqlx::migrate::MigrateError,
+    ),
+
+    #[error("Invalid map data request: {0}")]
+    InvalidMapDataRequest(
+        #[ts(skip)]
+        #[from]
+        axum::extract::rejection::QueryRejection,
+    ),
+
+    #[error("URL parse error: {0}")]
+    UrlParseError(
+        #[ts(skip)]
+        #[from]
+        url::ParseError,
+    ),
+
+    #[error("Session error: {0}")]
+    SessionError(
+        #[ts(skip)]
+        #[from]
+        tower_sessions::session::Error,
+    ),
+
+    #[error("Axum error: {0}")]
+    AxumError(
+        #[ts(skip)]
+        #[from]
+        axum::http::Error,
+    ),
+
+    #[error("Request to Nadeo API failed")]
+    NadeoApiFailed(
+        #[ts(skip)]
+        #[from]
+        reqwest::Error,
+    ),
+
+    #[error("Oauth failed: {0}")]
+    OauthFailed(#[ts(skip)] String),
+
+    #[error("Invalid OAuth request: {0}")]
+    InvalidOauth(
+        #[ts(skip)]
+        #[from]
+        axum::extract::rejection::JsonRejection,
+    ),
+
+    #[error("Map not found: {0}")]
+    MapNotFound(#[ts(skip)] u64),
+
+    #[error("No such API: {0}")]
+    NotFound(#[ts(skip)] String),
+}
+
+#[derive(Debug)]
+pub enum ApiError {
+    Root(ApiErrorInner),
+    Context {
+        context: String,
+        inner: Box<ApiError>,
+    },
+}
+
+#[derive(Serialize, TS)]
+#[ts(export)]
+#[serde(tag = "type")]
+struct TsApiError {
+    #[ts(as = "ApiErrorInner")]
+    error: String,
+    message: String,
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        tracing::error!("{}", self);
+
+        let error: &'static str = (&*self).into();
+        let status_code = match &*self {
+            ApiErrorInner::Database(_)
+            | ApiErrorInner::Migration(_)
+            | ApiErrorInner::UrlParseError(_)
+            | ApiErrorInner::SessionError(_)
+            | ApiErrorInner::AxumError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ApiErrorInner::NadeoApiFailed(_) => StatusCode::BAD_GATEWAY,
+            ApiErrorInner::InvalidMapDataRequest(_)
+            | ApiErrorInner::OauthFailed(_)
+            | ApiErrorInner::InvalidOauth(_) => StatusCode::BAD_REQUEST,
+            ApiErrorInner::MapNotFound(_) | ApiErrorInner::NotFound(_) => StatusCode::NOT_FOUND,
+        };
+
+        (
+            status_code,
+            Json(TsApiError {
+                error: error.to_owned(),
+                message: self.to_string(),
+            }),
+        )
+            .into_response()
+    }
+}
+
+impl std::error::Error for ApiError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        let inner: &ApiErrorInner = &*self;
+        Some(inner)
+    }
+}
+
+pub trait Context<T> {
+    fn context<C>(self, context: C) -> Result<T, ApiError>
+    where
+        C: std::fmt::Display + Send + Sync + 'static;
+
+    fn with_context<F, C>(self, context_fn: F) -> Result<T, ApiError>
+    where
+        F: FnOnce() -> C,
+        C: std::fmt::Display + Send + Sync + 'static;
+}
+
+impl<T, E: Into<ApiError>> Context<T> for Result<T, E> {
+    fn context<C>(self, context: C) -> Result<T, ApiError>
+    where
+        C: std::fmt::Display + Send + Sync + 'static,
+    {
+        match self {
+            Ok(t) => Ok(t),
+            Err(err) => Err(ApiError::Context {
+                context: context.to_string(),
+                inner: Box::new(err.into()),
+            }),
+        }
+    }
+
+    fn with_context<F, C>(self, context_fn: F) -> Result<T, ApiError>
+    where
+        F: FnOnce() -> C,
+        C: std::fmt::Display + Send + Sync + 'static,
+    {
+        match self {
+            Ok(t) => Ok(t),
+            Err(err) => Err(ApiError::Context {
+                context: context_fn().to_string(),
+                inner: Box::new(err.into()),
+            }),
+        }
+    }
+}
+
+impl Deref for ApiError {
+    type Target = ApiErrorInner;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            ApiError::Root(inner) => inner,
+            ApiError::Context { inner, .. } => {
+                let box_ref = Box::as_ref(inner);
+                <ApiError as Deref>::deref(box_ref)
+            }
+        }
+    }
+}
+
+impl<T: Into<ApiErrorInner>> From<T> for ApiError {
+    fn from(value: T) -> Self {
+        ApiError::Root(value.into())
+    }
+}
+
+impl Display for ApiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ApiError::Root(inner) => write!(f, "{}", inner),
+            ApiError::Context { context, inner } => {
+                Display::fmt(inner, f)?;
+                write!(f, "\n  {}", context)
+            }
+        }
+    }
+}
