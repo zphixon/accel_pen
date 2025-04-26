13d1b8cb111e1f06fcb8e844be2ef62b6e241760
Zack H <zphixon@gmail.com>
2025-04-26 16:07:31 -0400

    move everything to session

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,30 +1,14 @@
 use axum::{
-    body::Body,
-    error_handling::HandleErrorLayer,
-    extract::{
-        rejection::{JsonRejection, QueryRejection},
-        Path, Query, Request, State,
-    },
-    http::{header, HeaderValue, StatusCode, Uri},
-    middleware::{self, Next},
-    response::{AppendHeaders, IntoResponse, Redirect, Response},
-    routing::{get, post},
-    BoxError, Json, Router,
+    extract::{Query, State},
+    http::{header, HeaderValue, Uri},
+    response::Redirect,
+    routing::get,
+    Json, Router,
 };
 use axum_extra::extract::WithRejection;
-use config::{CLIENT_SECRET, CONFIG};
-use error::{ApiError, ApiErrorInner, Context};
 use serde::{Deserialize, Serialize};
 use sqlx::MySqlPool;
-use std::{
-    collections::HashMap,
-    fmt::Display,
-    net::SocketAddr,
-    ops::Deref,
-    path::PathBuf,
-    sync::{Arc, LazyLock},
-};
-use tokio::{net::TcpListener, sync::RwLock};
+use tokio::net::TcpListener;
 use tower_http::cors::{self, CorsLayer};
 use tower_sessions::{
     cookie::{time::Duration, Key, SameSite},
@@ -37,12 +21,10 @@ use uuid::Uuid;
 mod config;
 mod error;
 
-struct Login {
-    csrf_token: Uuid,
-    nadeo_oauth: NadeoOauth,
-}
+use config::{CLIENT_SECRET, CONFIG};
+use error::{ApiError, ApiErrorInner, Context};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct NadeoOauth {
     token_type: String,
     expires_in: u64,
@@ -53,7 +35,6 @@ struct NadeoOauth {
 #[derive(Clone)]
 struct AppState {
     pool: MySqlPool,
-    sessions: Arc<RwLock<HashMap<Uuid, Login>>>,
 }
 
 #[tokio::main]
@@ -84,10 +65,7 @@ async fn main() -> anyhow::Result<()> {
         .route(&CONFIG.route_v1("map_data"), get(map_data))
         .route(&CONFIG.route_v1("oauth/start"), get(oauth_start))
         .route(&CONFIG.route_v1("oauth/finish"), get(oauth_finish))
-        .with_state(AppState {
-            pool,
-            sessions: Arc::new(RwLock::new(HashMap::new())),
-        })
+        .with_state(AppState { pool })
         .fallback(fallback)
         .layer(
             CorsLayer::new()
@@ -108,15 +86,9 @@ async fn fallback(uri: Uri) -> ApiError {
 }
 
 const RANDOM_STATE_KEY: &str = "randomState";
-const SESSSION_ID_KEY: &str = "sessionId";
-const CSRF_TOKEN_KEY: &str = "csrfToken";
+const NADEO_TOKENS_KEY: &str = "nadeoTokens";
 
 async fn oauth_start(session: Session) -> Result<Redirect, ApiError> {
-    session
-        .insert(SESSSION_ID_KEY, Uuid::new_v4())
-        .await
-        .context("Setting extra entropy on session")?;
-
     let state = Uuid::new_v4();
     session
         .insert(RANDOM_STATE_KEY, state)
@@ -146,20 +118,9 @@ struct OauthFinishRequest {
 }
 
 async fn oauth_finish(
-    State(state): State<AppState>,
     session: Session,
     WithRejection(Query(request), _): WithRejection<Query<OauthFinishRequest>, ApiError>,
 ) -> Result<Redirect, ApiError> {
-    let Some(session_id) = session
-        .get::<Uuid>(SESSSION_ID_KEY)
-        .await
-        .context("Reading session ID from session")?
-    else {
-        return Err(
-            ApiErrorInner::OauthFailed(String::from("Missing session ID in session")).into(),
-        );
-    };
-
     let Some(session_state) = session
         .get::<Uuid>(RANDOM_STATE_KEY)
         .await
@@ -204,36 +165,21 @@ async fn oauth_finish(
         .context("Sending request for access token")?;
 
     if response.status().is_success() {
-        let nadeo_oauth = response.json().await?;
+        let nadeo_oauth: NadeoOauth = response
+            .json()
+            .await
+            .context("Parsing oauth tokens from Nadeo")?;
 
-        let csrf_token = Uuid::new_v4();
-        state.sessions.write().await.insert(
-            session_id,
-            Login {
-                csrf_token,
-                nadeo_oauth,
-            },
-        );
+        session
+            .insert(NADEO_TOKENS_KEY, nadeo_oauth)
+            .await
+            .context("Writing oauth tokens to session")?;
 
-        Ok(Redirect::to(
-            Url::parse_with_params(
-                CONFIG.net.frontend_url.as_str(),
-                &[(CSRF_TOKEN_KEY, csrf_token.hyphenated().to_string().as_str())],
-            )
-            .context("Creating redirect URL back to frontend")?
-            .as_str(),
-        ))
+        Ok(Redirect::to(CONFIG.net.frontend_url.as_str()))
     } else {
         let json_error: serde_json::Value = response.json().await?;
         Err(ApiErrorInner::OauthFailed(format!("{}", json_error)).into())
     }
-}
-
-#[derive(Serialize, TS)]
-#[ts(export)]
-#[serde(tag = "type")]
-struct MapDataResponse {
-    name: String,
 }
 
 #[derive(Deserialize, TS)]
@@ -241,6 +187,13 @@ struct MapDataResponse {
 #[serde(tag = "type")]
 struct MapDataRequest {
     id: u64,
+}
+
+#[derive(Serialize, TS)]
+#[ts(export)]
+#[serde(tag = "type")]
+struct MapDataResponse {
+    name: String,
 }
 
 async fn map_data(
