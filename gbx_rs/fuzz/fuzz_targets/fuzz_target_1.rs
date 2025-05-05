9b445f05a716bab4701cc10e4d091f739044e6ca
Zack H <zphixon@gmail.com>
2025-05-04 22:26:14 -0400

    hmm

--- gbx_rs/fuzz/fuzz_targets/fuzz_target_1.rs
+++ gbx_rs/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -2,7 +2,7 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok(node) = gbx_rs::Node::read_from(data) {
+    if let Ok(node) = gbx_rs::Node::read_from(&data) {
         let _ = node.parse();
     }
 });
