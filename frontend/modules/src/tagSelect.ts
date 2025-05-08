a6440d5482632f3ec8d41f2a9cb92b987cfe3bcb
Zack H <zphixon@gmail.com>
2025-05-08 19:43:26 -0400

    add max selected tags

--- frontend/modules/src/tagSelect.ts
+++ frontend/modules/src/tagSelect.ts
@@ -1,15 +1,36 @@
 export let selectedTagList = document.getElementById("selectedTagList")!;
 export let tagCheckboxes: NodeListOf<HTMLInputElement> = document.querySelectorAll(".tagCheckbox");
+export let maxSelected = 7;
 
+let selected = 0;
 for (let checkbox of tagCheckboxes) {
   checkbox.checked = false;
   let label = checkbox.parentElement!;
   let container = label.parentElement!;
-  checkbox.onchange = _ => {
+  checkbox.onchange = ev => {
+    ev.preventDefault();
+
     if (checkbox.checked) {
+      selected += 1;
+    } else {
+      selected = Math.max(0, selected - 1);
+    }
+
+    if (selected > maxSelected) {
+      selected -= 1;
+      container.appendChild(label);
+      checkbox.checked = false;
+    } else if (checkbox.checked) {
       selectedTagList.appendChild(label);
     } else {
       container.appendChild(label);
+    }
+
+    let tagSelectGrid: HTMLElement = document.querySelector(".tagSelectGrid")!;
+    if (selected == maxSelected) {
+      tagSelectGrid.classList.add("fullSelection");
+    } else {
+      tagSelectGrid.classList.remove("fullSelection");
     }
   };
 }
@@ -21,4 +42,3 @@ document.getElementById("resetTags")!.onclick = _ => {
     checkbox.dispatchEvent(event);
   }
 };
-
