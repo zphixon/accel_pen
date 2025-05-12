b18ed0e16698ff272770deb8367a010e4fbf04b9
Zack H <zphixon@gmail.com>
2025-05-11 23:18:14 -0400

    reduce query duplication

--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,7 @@
 use crate::{
     api::{TagInfo, UserResponse},
     config,
-    error::Context,
+    error::{ApiError, Context},
     nadeo::{self, auth::NadeoAuthSession},
     AppState,
 };
@@ -16,9 +16,9 @@ use ts_rs::TS;
 
 #[derive(Serialize, TS)]
 #[ts(export)]
-struct MapContext<'auth> {
+struct MapContext {
     id: i32,
-    gbx_uid: &'auth str,
+    gbx_uid: String,
     plain_name: String,
     name: nadeo::FormattedString,
     votes: i32,
@@ -92,11 +92,11 @@ pub async fn index(State(state): State<AppState>, auth: Option<NadeoAuthSession>
         {
             Ok(my_maps) => {
                 let mut maps_context = Vec::new();
-                for map in my_maps.iter() {
+                for map in my_maps {
                     let name = nadeo::FormattedString::parse(&map.mapname);
                     maps_context.push(MapContext {
                         id: map.ap_map_id,
-                        gbx_uid: &map.gbx_mapuid,
+                        gbx_uid: map.gbx_mapuid,
                         plain_name: name.strip_formatting(),
                         name,
                         votes: map.votes,
@@ -147,11 +147,11 @@ pub async fn index(State(state): State<AppState>, auth: Option<NadeoAuthSession>
     {
         Ok(recent_rows) => {
             let mut recent_maps = Vec::new();
-            for map in recent_rows.iter() {
+            for map in recent_rows {
                 let name = nadeo::FormattedString::parse(&map.mapname);
                 recent_maps.push(MapContext {
                     id: map.ap_map_id,
-                    gbx_uid: &map.gbx_mapuid,
+                    gbx_uid: map.gbx_mapuid,
                     plain_name: name.strip_formatting(),
                     name,
                     votes: map.votes,
@@ -195,14 +195,12 @@ pub async fn index(State(state): State<AppState>, auth: Option<NadeoAuthSession>
     }
 }
 
-pub async fn map_page(
-    State(state): State<AppState>,
-    auth: Option<NadeoAuthSession>,
-    Path(map_id): Path<i32>,
-) -> Response {
-    let mut context = config::context_with_auth_session(auth.as_ref());
-
-    let map = match sqlx::query!(
+async fn populate_context_with_map_data(
+    state: &AppState,
+    map_id: i32,
+    context: &mut TeraContext,
+) -> Result<Option<MapContext>, ApiError> {
+    let map = sqlx::query!(
         "
             SELECT map.ap_map_id, map.gbx_mapuid, map.mapname, map.votes, map.uploaded,
                 map.ap_author_id, ap_user.nadeo_display_name, ap_user.ap_user_id, ap_user.nadeo_id,
@@ -215,22 +213,10 @@ pub async fn map_page(
     )
     .fetch_optional(&state.pool)
     .await
-    {
-        Ok(maybe_row) => maybe_row,
-        Err(err) => {
-            return render_error(
-                &state,
-                context,
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Database error reading map data",
-                err,
-            );
-        }
-    };
+    .context("Reading map from database")?;
 
-    let had_map = map.is_some();
     if let Some(map) = map {
-        let tags = match sqlx::query!(
+        let tags = sqlx::query!(
             "
             SELECT tag_name.tag_id, tag_name.tag_name, tag_name.tag_kind
             FROM tag_name
@@ -243,18 +229,7 @@ pub async fn map_page(
         )
         .fetch_all(&state.pool)
         .await
-        {
-            Ok(tags) => tags,
-            Err(err) => {
-                return render_error(
-                    &state,
-                    context,
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    "Database error reading map tags",
-                    err,
-                )
-            }
-        }
+        .context("Reading tags from map")?
         .into_iter()
         .map(|row| TagInfo {
             id: row.tag_id,
@@ -264,39 +239,62 @@ pub async fn map_page(
         .collect::<Vec<_>>();
 
         let name = nadeo::FormattedString::parse(&map.mapname);
-        context.insert(
-            "map",
-            &MapContext {
-                id: map.ap_map_id,
-                gbx_uid: &map.gbx_mapuid,
-                plain_name: name.strip_formatting(),
-                name,
-                votes: map.votes,
-                uploaded: map
-                    .uploaded
-                    .format(&time::format_description::well_known::Iso8601::DATE_TIME_OFFSET)
-                    .unwrap(),
-                author: UserResponse {
-                    display_name: map.nadeo_display_name,
-                    account_id: map.nadeo_id,
-                    user_id: map.ap_user_id,
-                    club_tag: map
-                        .nadeo_club_tag
-                        .as_deref()
-                        .map(nadeo::FormattedString::parse),
-                },
-                tags,
-                medals: Some(Medals {
-                    author: map.author_medal_ms as u32,
-                    gold: map.gold_medal_ms as u32,
-                    silver: map.silver_medal_ms as u32,
-                    bronze: map.bronze_medal_ms as u32,
-                }),
+
+        let map_context = MapContext {
+            id: map.ap_map_id,
+            gbx_uid: map.gbx_mapuid,
+            plain_name: name.strip_formatting(),
+            name,
+            votes: map.votes,
+            uploaded: map
+                .uploaded
+                .format(&time::format_description::well_known::Iso8601::DATE_TIME_OFFSET)
+                .unwrap(),
+            author: UserResponse {
+                display_name: map.nadeo_display_name,
+                account_id: map.nadeo_id,
+                user_id: map.ap_user_id,
+                club_tag: map
+                    .nadeo_club_tag
+                    .as_deref()
+                    .map(nadeo::FormattedString::parse),
             },
-        );
+            tags,
+            medals: Some(Medals {
+                author: map.author_medal_ms as u32,
+                gold: map.gold_medal_ms as u32,
+                silver: map.silver_medal_ms as u32,
+                bronze: map.bronze_medal_ms as u32,
+            }),
+        };
+
+        context.insert("map", &map_context);
+
+        Ok(Some(map_context))
     } else {
-        context.insert("map", &None::<()>);
+        Ok(None)
     }
+}
+
+pub async fn map_page(
+    State(state): State<AppState>,
+    auth: Option<NadeoAuthSession>,
+    Path(map_id): Path<i32>,
+) -> Response {
+    let mut context = config::context_with_auth_session(auth.as_ref());
+
+    let map = match populate_context_with_map_data(&state, map_id, &mut context).await {
+        Ok(had_map) => had_map,
+        Err(error) => {
+            return render_error(
+                &state,
+                context,
+                StatusCode::INTERNAL_SERVER_ERROR,
+                error,
+                "Fetching map from database",
+            )
+        }
+    };
 
     match state
         .tera
@@ -306,7 +304,7 @@ pub async fn map_page(
         .context("Rendering map page template")
     {
         Ok(page) => (
-            if had_map {
+            if map.is_some() {
                 StatusCode::OK
             } else {
                 StatusCode::NOT_FOUND
@@ -316,6 +314,29 @@ pub async fn map_page(
             .into_response(),
         Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response(),
     }
+}
+
+async fn populate_context_with_tags(
+    state: &AppState,
+    context: &mut TeraContext,
+) -> Result<(), ApiError> {
+    let tag_names = sqlx::query!("SELECT tag_id, tag_name, tag_kind FROM tag_name")
+        .fetch_all(&state.pool)
+        .await
+        .context("Getting tag names")?;
+
+    let tag_names = tag_names
+        .into_iter()
+        .map(|row| TagInfo {
+            id: row.tag_id,
+            name: row.tag_name,
+            kind: row.tag_kind,
+        })
+        .collect::<Vec<TagInfo>>();
+
+    context.insert("tags", &tag_names);
+
+    Ok(())
 }
 
 pub async fn map_manage_page(
@@ -334,130 +355,39 @@ pub async fn map_manage_page(
         );
     };
 
-    let map = match sqlx::query!(
-        "
-            SELECT map.ap_map_id, map.gbx_mapuid, map.mapname, map.votes, map.uploaded,
-                map.ap_author_id, ap_user.nadeo_display_name, ap_user.ap_user_id, ap_user.nadeo_id,
-                ap_user.nadeo_club_tag
-            FROM map JOIN ap_user ON map.ap_author_id = ap_user.ap_user_id
-            WHERE map.ap_map_id = $1
-        ",
-        map_id,
-    )
-    .fetch_optional(&state.pool)
-    .await
-    {
-        Ok(maybe_row) => maybe_row,
-        Err(err) => {
-            return render_error(
-                &state,
-                context,
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Database error reading map data",
-                err,
-            );
-        }
-    };
-
-    if let Some(map) = map {
-        if map.ap_author_id != auth.user_id() {
+    match populate_context_with_map_data(&state, map_id, &mut context).await {
+        Ok(Some(map)) if map.author.user_id != auth.user_id() => {
             return render_error(
                 &state,
                 context,
                 StatusCode::UNAUTHORIZED,
-                "Not allowed",
-                "Cannot manage other users' maps",
+                "Unauthorized",
+                "This isn't your map >:(",
             );
         }
 
-        let tags = match sqlx::query!(
-            "
-            SELECT tag_name.tag_id, tag_name.tag_name, tag_name.tag_kind
-            FROM tag_name
-            JOIN tag ON tag.tag_id = tag_name.tag_id
-            JOIN map ON tag.ap_map_id = $1
-            GROUP BY tag_name.tag_id
-            ORDER BY tag_name.tag_id ASC
-        ",
-            map.ap_map_id
-        )
-        .fetch_all(&state.pool)
-        .await
-        {
-            Ok(tags) => tags,
-            Err(err) => {
-                return render_error(
-                    &state,
-                    context,
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    "Database error reading map tags",
-                    err,
-                )
-            }
-        }
-        .into_iter()
-        .map(|row| TagInfo {
-            id: row.tag_id,
-            name: row.tag_name,
-            kind: row.tag_kind,
-        })
-        .collect::<Vec<_>>();
+        Ok(_) => {}
 
-        let name = nadeo::FormattedString::parse(&map.mapname);
-        context.insert(
-            "map",
-            &MapContext {
-                id: map.ap_map_id,
-                gbx_uid: &map.gbx_mapuid,
-                plain_name: name.strip_formatting(),
-                name,
-                votes: map.votes,
-                uploaded: map
-                    .uploaded
-                    .format(&time::format_description::well_known::Iso8601::DATE_TIME_OFFSET)
-                    .unwrap(),
-                author: UserResponse {
-                    display_name: map.nadeo_display_name,
-                    account_id: map.nadeo_id,
-                    user_id: map.ap_user_id,
-                    club_tag: map
-                        .nadeo_club_tag
-                        .as_deref()
-                        .map(nadeo::FormattedString::parse),
-                },
-                tags,
-                medals: None,
-            },
-        );
-    } else {
-        context.insert("map", &None::<()>);
-    }
-
-    let tag_names = match sqlx::query!("SELECT tag_id, tag_name, tag_kind FROM tag_name")
-        .fetch_all(&state.pool)
-        .await
-    {
-        Ok(tag_names) => tag_names,
-        Err(err) => {
+        Err(error) => {
             return render_error(
                 &state,
                 context,
                 StatusCode::INTERNAL_SERVER_ERROR,
-                "Database error getting tag names",
-                err,
-            );
+                error,
+                "Fetching map from database",
+            )
         }
-    };
+    }
 
-    let tag_names = tag_names
-        .into_iter()
-        .map(|row| TagInfo {
-            id: row.tag_id,
-            name: row.tag_name,
-            kind: row.tag_kind,
-        })
-        .collect::<Vec<TagInfo>>();
-    context.insert("tags", &tag_names);
+    if let Err(error) = populate_context_with_tags(&state, &mut context).await {
+        return render_error(
+            &state,
+            context,
+            StatusCode::INTERNAL_SERVER_ERROR,
+            error,
+            "Getting tags from database",
+        );
+    }
 
     match state
         .tera
@@ -474,31 +404,18 @@ pub async fn map_manage_page(
 pub async fn map_upload(State(state): State<AppState>, auth: Option<NadeoAuthSession>) -> Response {
     let mut context = config::context_with_auth_session(auth.as_ref());
 
-    let tag_names = match sqlx::query!("SELECT tag_id, tag_name, tag_kind FROM tag_name")
-        .fetch_all(&state.pool)
-        .await
-    {
-        Ok(tag_names) => tag_names,
+    match populate_context_with_tags(&state, &mut context).await {
+        Ok(_) => {}
         Err(err) => {
             return render_error(
                 &state,
                 context,
                 StatusCode::INTERNAL_SERVER_ERROR,
-                "Database error getting tag names",
                 err,
+                "Getting tags from database",
             );
         }
-    };
-
-    let tag_names = tag_names
-        .into_iter()
-        .map(|row| TagInfo {
-            id: row.tag_id,
-            name: row.tag_name,
-            kind: row.tag_kind,
-        })
-        .collect::<Vec<TagInfo>>();
-    context.insert("tags", &tag_names);
+    }
 
     match state
         .tera
