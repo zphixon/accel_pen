0c04e2e89928d0bc88016ddf2c24edf80aa24504
Zack H <zphixon@gmail.com>
2025-05-14 00:03:51 -0400

    not shit tag select component

--- frontend/modules/src/components/TagSelect.tsx
+++ frontend/modules/src/components/TagSelect.tsx
@@ -1,32 +1,9 @@
+import Select, { MultiValueProps, OptionProps, components } from "react-select";
 import * as types from "../bindings/index";
 
-interface TagBadgeProps {
-  tag: types.TagInfo,
-  checked: boolean,
-  selected: boolean,
-  onChange?: React.ChangeEventHandler<HTMLInputElement>,
-}
-function TagBadge({ tag, checked, selected, onChange }: TagBadgeProps) {
-  let root = tag.name.split("/")[0];
-  let rest = tag.name.split("/").slice(1).join("/");
-  return <>
-    <div className={["tagBadge", root].join(" ")}>
-      <input
-        hidden
-        className={["tagCheckbox", selected ? "tagCheckboxSelected" : ""].join(" ")}
-        type="checkbox"
-        id={tag.name}
-        onChange={onChange}
-        checked={checked}
-      />
-      <label
-        htmlFor={tag.name}
-        className={["tagName", root].join(" ")}
-      >
-        <span className="rootName">{root}/</span>{rest}
-      </label>
-    </div>
-  </>;
+interface TagOption {
+  value: types.TagInfo,
+  label: string,
 }
 
 interface TagSelectProps {
@@ -38,55 +15,46 @@ interface TagSelectProps {
   maySelectTags?: boolean,
 }
 function TagSelect({ tagInfo, selectedTags, setSelectedTags, originalSelectedTags = [], maySelectTags = true, maxTags }: TagSelectProps) {
-  function toggleTag(event: React.ChangeEvent<HTMLInputElement>) {
-    if (event.target.checked) {
-      if (selectedTags.length >= maxTags || !maySelectTags) {
-        event.target.checked = false;
-        return;
-      }
+  let options = tagInfo.map(tag => ({value: tag, label: tag.name} as TagOption));
+  let selected = options.filter(tag => selectedTags.find(selectedTag => tag.value.id == selectedTag.id));
 
-      let newSelectedTags = structuredClone(selectedTags);
-      newSelectedTags.push(tagInfo.find(tag => tag.name == event.target.id)!);
-      setSelectedTags(newSelectedTags);
-    } else {
-      let newSelectedTags = structuredClone(selectedTags);
-      let rm = newSelectedTags.findIndex(tag => tag.name == event.target.id)!;
-      newSelectedTags.splice(rm, 1);
-      setSelectedTags(newSelectedTags);
-    }
-  }
-
-  let selectedTagList = selectedTags.map(tag => <TagBadge key={tag.id} tag={tag} selected={false} checked={true} onChange={toggleTag} />);
-  let tagGrid = tagInfo.map(tag => <div key={tag.id} className="tagContainer">
-    {selectedTags.find(selectedTag => selectedTag.name == tag.name)
-      ? <TagBadge key={tag.id} tag={tag} selected={true} checked={false} onChange={toggleTag} />
-      : <TagBadge key={tag.id} tag={tag} selected={false} checked={false} onChange={toggleTag} />}
-  </div>);
-
-  let gridClasses = ["tagList", "tagSelectGrid"];
-  if (selectedTags.length >= maxTags || !maySelectTags) {
-    gridClasses.push("fullSelection");
-  }
-
-  let buttonText = "Reset tags";
-  if (originalSelectedTags.length == 0) {
-    buttonText = "Clear tags";
-  }
-
-  return <div id="tagSelect">
-    <div id="tagSelectHeader">
-      <div id="selectedTagsContainer">
-        Selected tags: <span className="tagList" id="selectedTagList">{selectedTagList}</span>
-      </div>
-      <div id="resetTagsContainer">
-        <button id="resetTags" onClick={_ => setSelectedTags(originalSelectedTags)}>{buttonText}</button>
-      </div>
+  return <>
+    <div className="tagSelectContainer">
+      <Select
+        className="tagSelect"
+        isMulti={true}
+        isClearable={false}
+        closeMenuOnSelect={false}
+        hideSelectedOptions={false}
+        options={options}
+        value={selected}
+        onChange={(newValue, _meta) => {
+          setSelectedTags(newValue.map(value => value.value));
+        }}
+        classNames={{
+          multiValue: (state) => ["selectMultiValue", state.data.value.name.split("/")[0]].join(" "),
+          //option: (state) => state.isSelected ? "" : ["tagName", state.data.value.name.split("/")[0]].join(" "),
+        }}
+        components={{
+          MultiValue: (props: MultiValueProps<TagOption>) => <components.MultiValue {...props}>
+            <span className={["tagName", props.data.value.name.split("/")[0]].join(" ")}>
+              <span className="rootName">{props.data.value.name.split("/")[0]}</span>/{props.data.value.name.split("/").slice(1)}
+            </span>
+          </components.MultiValue>,
+          Option: (props: OptionProps<TagOption>) => <>
+            <components.Option {...props}>
+              <span className={["tagName", props.data.value.name.split("/")[0]].join(" ")}>
+                <span className="rootName">{props.data.value.name.split("/")[0]}</span>/{props.data.value.name.split("/").slice(1)}
+              </span>
+            </components.Option>
+          </>
+        }}
+      />
+      <button onClick={_ => setSelectedTags(originalSelectedTags)}>
+        {originalSelectedTags.length == 0 ? "Clear" : "Reset"}
+      </button>
     </div>
-    <br/>
-    <div className={gridClasses.join(" ")}>
-      {tagGrid}
-    </div>
-  </div>;
+  </>;
 }
 
 export default TagSelect
